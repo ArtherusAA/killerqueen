0ca990923e81353c5ae969ff99428879065d04d5
Sokolok003 <49098530+Sokolok003@users.noreply.github.com>
2019-04-27 19:48:19 +0300

    MORE COMMENTS MOOOOOOOOOOOORE!
    Don't worry about this, i just shoulde write all comments and remake this code, but i don't know how and just was writing comments for my beautiful crazy stupid code. So, we have very good team of 3 guys, oh no, sorry, of 5 guys, but know we are only 3 on this good (my favourite) lesson, and i have mp3 player of music. If you want, but i didn't ask you and just can't know, now my music is Leningrad and 55X55. It's very musicle songs. So, i just boring of writing this cooment of commit. Bye!)

--- Desktop/killergame/killer/views.py
+++ Desktop/killergame/killer/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render
 import killer.DataBaseControl as db
-import squilt3
+import squilt3 
 # Create your views here.
 
-conn = sqlite3.connect("killer.DataBaseControl.db")
-cursor = conn.cursor()
+conn = sqlite3.connect("scoreboard.sqlite3") # создаем соединение с базой данной
+cursor = conn.cursor() # создаем курсор для работы
 
 
 def add_user(name):
-    cursor.execute("""INSERT INTO players VALUES(name, kills, wins)""", (name, '0', '0'))
-    cursor.commit()
+    cursor.execute("""INSERT INTO players VALUES(?, ?, ?)""", (name, '0', '0')) # add new playes to scroboard
+    cursor.commit() # commit
 
 
 def set_kills(name, kills):
-    sql = "SELECT * FROM players WHERE player =?"
-    result = cursor.execute(sql, [(name)]) 
-    sql = """UPDATE players SET player = kills WHERE player = result.kills"""
-    cursor.execute(sql)
-    conn.commit()
+    sql = "SELECT * FROM players WHERE player = ?" # find player in table
+    result = cursor.execute(sql, [(name)]) # result from sql with name
+    sql = ("""UPDATE players SET player = ? WHERE player = ?""", (kills, result.kills)) # set players kills 
+    cursor.execute(sql) # ending
+    conn.commit() # commit
 
 
 def set_wins(name, wins):
-    sql = "SELECT * FROM players WHERE player =?"
-    result = cursor.execute(sql, [(name)]) 
-    sql = """UPDATE players SET player = wins WHERE player = result.wins"""
-    cursor.execute(sql)
-    conn.commit()
+    sql = "SELECT * FROM players WHERE player = ?" # find player in table
+    result = cursor.execute(sql, [(name)]) # result from sql with name
+    sql = ("""UPDATE players SET player = ? WHERE player = ?""", (wins, result.wins)) # set players wins
+    cursor.execute(sql) # ending
+    conn.commit() # commit
 
-conn.close()
+conn.close() # close cursor and connected because we had been ended to work with this f**king table
