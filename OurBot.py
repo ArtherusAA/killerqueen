ddcb05ef15253f2d77c3aa693789bc2fccdca425
qweqe <qweqe.asd@gmail.com>
2019-05-10 23:31:11 +0300

    make game play

--- OurBot.py
+++ OurBot.py
@@ -10,7 +10,7 @@ class BotHandler:
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-    def get_updates(self, offset=None, timeout=30):
+    def get_updates(self, offset=None, timeout=1000):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
@@ -66,6 +66,26 @@ def make_game():
         ans += chr(random.randint(65, 91))
     return ans
 
+def get_victim(game, victim, k = 0):
+    if(k != len(game)):
+        if (victim == None):
+            victim = []
+
+        killer = ''
+        while (get_user_target(killer) == 'free'):
+            killer = game[random.randint(0, len(game) - 1)]
+
+        zhertva = ''
+        while (zhertva == killer or zhertva in game):
+            zhertva = game[random.randint(0, len(game) - 1)]
+        victim.append(zhertva)
+        get_user_target(killer, zhertva)
+        k += 1
+        get_victim(game, victim, k)
+
+    else:
+        return
+
 def main():
     new_offset = None
     today = now.day
@@ -74,78 +94,90 @@ def main():
     print('Запуск успешен')
 
     while True:
-        try:
+        #try:
+        #print('Запуск успешен')
+        greet_bot.get_updates(new_offset)
+        last_update = greet_bot.get_last_update()
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+###########################################################################
+        text = list(last_chat_text.lower().split())
+        #print(text)
+###########################################################################
+        if last_chat_name == 'Jonathan⚡️' and text[0] == '/last_mess':
+            last_mess(last_chat_id)
+###########################################################################
+        elif (text[0] == '/make_game'):
+            try:
+                if get_user_target(last_chat_id == 'free'): # what target?
+                    str = make_game()
+                    create_game(str)
+                    join_game(last_chat_id, str)
+                    greet_bot.send_message(last_chat_id, 'Отлично твоя игра создана, её уникальный номер: ' + str)
+                    set_target_to_user(last_chat_id, 'playing') #what target?
+                else:
+                    greet_bot.send_message(last_chat_id, 'Ты не можешь этого сделать, т.к. находишься в другой игре. Доиграй или покинь её')
+            except:
+                greet_bot.send_message(last_chat_id, 'Я такое не умею, может в следующий раз')
+###########################################################################
+        elif (text[0] == '/join'): # проверить существует ли такое лобби и не играют ли уже там
+            try:
+                game = text[len(text) - 1]
+                #if get_game(game == 'wait'): #what target? if game are going how i can check it?
+                if get_user_target(last_chat_id == 'free'): #what target?
+                    greet_bot.send_message(last_chat_id, 'Отлично сейчас я добавлю тебя в лобби: ' + game)
+                    join_game(last_chat_id, game)
+                    set_target_to_user(last_chat_id, 'playing') #what target?
+                else:
+                    greet_bot.send_message(last_chat_id, 'Ты не можешь этого сделать, т.к. находишься в другой игре. Доиграй или покинь её')
+                # else:
+                #     greet_bot.send_message(last_chat_id, 'Ты не можешь этого сделать, т.к. такой игры нет или она уже идёт(((')
+            except:
+                greet_bot.send_message(last_chat_id, 'Я такое не умею, может в следующий раз')
 
-            greet_bot.get_updates(new_offset)
-            last_update = greet_bot.get_last_update()
-            last_update_id = last_update['update_id']
-            last_chat_text = last_update['message']['text']
-            last_chat_id = last_update['message']['chat']['id']
-            last_chat_name = last_update['message']['chat']['first_name']
+        elif (text[0] == 'start_game'):
+            try:
+                for i in get_game(last_chat_id):
+                    greet_bot.send_message(i, 'Игра начинается!')
+
+                get_victim(get_players(get_game(last_chat_id)), None, 0)
+
+                for i in get_game(last_chat_id):
+                    greet_bot.send_message(i, 'Игра начилась, Удачи!')
+            except:
+                print('Error')
 ###########################################################################
-            text = list(last_chat_text.lower().split())
+        elif (text[0] == '/add'):
+            try:
+                text = map(int, text[1:])
+                greet_bot.send_message(last_chat_id, 'Сумма чисел, {}'.format(sum(text)))
+            except:
+                greet_bot.send_message(last_chat_id, 'Ой ОЙ оЙ, за такое положен бан')
 ###########################################################################
-            if last_chat_name == 'Jonathan⚡️' and text[0] == '/last_mess':
-                last_mess(last_chat_id)
-###########################################################################
-            elif (text[0] == '/make_game'):
-                try:
-                    if get_user_target(last_chat_id == ): # what target?
-                        str = make_game()
-                        create_game(str)
-                        join_game(last_chat_id, str)
-                        greet_bot.send_message(last_chat_id, 'Отлично твоя игра создана, её уникальный номер: ' + str)
-                        set_target_to_user(last_chat_id, ) #what target?
-                    else:
-                        greet_bot.send_message(last_chat_id, 'Ты не можешь этого сделать, т.к. находишься в
-                         другой игре. Доиграй или покинь её')
-                except:
-                    greet_bot.send_message(last_chat_id, 'Я такое не умею, может в следующий раз')
-###########################################################################
-            elif (text[0] == '/join'): # проверить существует ли такое лобби и не играют ли уже там
-                try:
-                    game = text[len(text) - 1]
-                    if get_game(game == ): #what target? if game are going how i can check it?
-                        if get_user_target(last_chat_id == ): #what target?
-                            greet_bot.send_message(last_chat_id, 'Отлично сейчас я добавлю тебя в лобби: ' + game)
-                            join_game(last_chat_id, game)
-                            set_target_to_user(last_chat_id, ) #what target?
-                        else:
-                            greet_bot.send_message(last_chat_id, 'Ты не можешь этого сделать, т.к. находишься в
-                             другой игре. Доиграй или покинь её')
-                    else:
-                        greet_bot.send_message(last_chat_id, 'Ты не можешь этого сделать, т.к. такой игры нет или она уже идёт(((')
-                except:
-                    greet_bot.send_message(last_chat_id, 'Я такое не умею, может в следующий раз')
-###########################################################################
-            # elif (text[0] == '/add'):
-            #     try:
-            #         text = map(int, text[1:])
-            #         greet_bot.send_message(last_chat_id, 'Сумма чисел, {}'.format(sum(text)))
-            #     except:
-            #         greet_bot.send_message(last_chat_id, 'Ой ОЙ оЙ, за такое положен бан')
-###########################################################################
-            elif text[0] == '/jesus':
-                try:
-                    f = open('/Users/jonathan/Documents/PromProg/FInal_proj/killergame/Jesus.txt', 'r')
-                    jesus = f.read()
-                    greet_bot.send_message(last_chat_id, jesus)
-                    f.close()
-                except:
-                    greet_bot.send_message(last_chat_id, 'Ой ОЙ оЙ, за такое положен бан')
+        elif text[0] == '/jesus':
+            try:
+                f = open('/Users/jonathan/Documents/PromProg/FInal_proj/killergame/Jesus.txt', 'r')
+                jesus = f.read()
+                greet_bot.send_message(last_chat_id, jesus)
+                f.close()
+            except:
+                greet_bot.send_message(last_chat_id, 'Ой ОЙ оЙ, за такое положен бан')
 ###########################################################################
 
-            file = open('/Users/jonathan/Documents/PromProg/FInal_proj/killergame/lastmes.txt', 'a')
-            file.write(last_chat_name + ' ' + last_chat_text + '\n')
-            file.close()
-            print(last_chat_name, last_chat_id, last_chat_text)# View who is write && what
-            #print(last_update['message'])
-            new_offset = last_update_id + 1
-        except:
-            print('Error')
+        file = open('/Users/jonathan/Documents/PromProg/FInal_proj/killergame/lastmes.txt', 'a')
+        file.write(last_chat_name + ' ' + last_chat_text + '\n')
+        file.close()
+        print(last_chat_name, last_chat_id, last_chat_text)# View who is write && what
+        #print(last_update['message'])
+        new_offset = last_update_id + 1
+        # except:
+        #     print('Error')
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        print('я вышел')
         exit()
