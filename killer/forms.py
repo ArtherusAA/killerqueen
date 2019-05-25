7d05fe6e87eb2c5f31da19030fddfcdcdd8d429c
sizzziy <sizzziy@yandex.ru>
2019-05-25 23:44:47 +0300

    Added pep8

--- killer/forms.py
+++ killer/forms.py
@@ -1,15 +1,27 @@
+"""
+standart django-forms
+"""
 from django import forms
 
 
 class AddForm(forms.Form):
+    """
+    Adding user form
+    """
     name = forms.CharField(max_length=100)
 
 
 class ChangeKillsForm(forms.Form):
+    """
+    Changing kills form
+    """
     name = forms.CharField(max_length=100)
     kills = forms.CharField(max_length=100)
 
 
 class ChangeWinsForm(forms.Form):
+    """
+    Changing wins form
+    """
     name = forms.CharField(max_length=100)
     wins = forms.CharField(max_length=100)
